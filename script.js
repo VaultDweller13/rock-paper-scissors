83c7b326b741dd7f27acbd37f7aee1c3cbddfd79
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2021-10-20 12:26:03 +0400

    Working game, using console output.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 let moves = ["rock", "paper", "scissors"];
-let playerSelection = prompt("Choose your play").toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     return moves[Math.floor(Math.random() * moves.length)];
@@ -13,12 +14,35 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")) {
             result = `You win! ${playerSelection} beats ${computerSelection}`;
+            playerScore++;
         }
     else if (playerSelection == computerSelection) {
         result = "It's a tie!";
     }
-    else result = `You lose! ${computerSelection} beats ${playerSelection}`;
+    else {
+      result = `You lose! ${computerSelection} beats ${playerSelection}`;
+      computerScore++;
+    }
 
-    console.log(playerSelection, computerSelection);
+    //console.log(playerSelection, computerSelection);
     return result;
 }
+
+function announceWinner() {
+  if (playerScore > computerScore) {
+    console.log(`You win! Score is ${playerScore + " : " + computerScore}`);
+  } else if (playerScore < computerScore) {
+      console.log(`You lose! Score is ${playerScore + " : " + computerScore}`);
+  } else console.log(`It's a tie! Score is ${playerScore + " : " + computerScore}`)
+}
+
+function game() {
+  playerScore = computerScore = 0;
+  
+  for(let i = 0; i < 5; i++) {
+    console.log(playRound(prompt("Choose your play").toLowerCase(), computerPlay()));
+  }
+  
+  announceWinner();
+}
+
