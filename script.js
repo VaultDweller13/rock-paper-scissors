de6c99469db7d4b1db88570ea98f6466d7993453
Artyom <sleva.sverxy@mail.ru>
2021-11-02 18:42:55 +0400

    added basic button UI

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 "use strict";
-let moves = ["rock", "paper", "scissors"];
-let playerSelection = prompt("Choose your play").toLowerCase();
+const moves = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll(".moves");
+let playerScore = 0;
+let computerScore = 0;
 
-function computerPlay() {
-    return moves[Math.floor(Math.random() * moves.length)];
+buttons.forEach(btn => btn.addEventListener('click', playRound));
+
+function playRound(e) {
+  const playerSelection = this.value;
+  const computerSelection = computerPlay();
+  const resultField = document.querySelector('#result');
+
+  let result = getResult(playerSelection, computerSelection);
+  
+  resultField.textContent = `Your play: ${playerSelection}; 
+                            Opponent play: ${computerSelection}. ${result}
+                            Player: ${playerScore} Opponent: ${computerScore}`;
+
+  if (playerScore >= 5) {
+    resultField.textContent = resultField.textContent.concat(` Congratulations! You win ${playerScore} rounds!`)
+    resetScore();
+  } else if (computerScore >= 5) {
+      resultField.textContent = resultField.textContent.concat(` What a shame! Computer wins ${computerScore} rounds!`)
+      resetScore();
+    }
 }
 
-function playRound(playerSelection, computerSelection) {
-    let result;
+function computerPlay() {
+  return moves[Math.floor(Math.random() * moves.length)];
+}
 
-    if ((playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")) {
-            result = `You win! ${playerSelection} beats ${computerSelection}`;
-        }
-    else if (playerSelection == computerSelection) {
-        result = "It's a tie!";
-    }
-    else result = `You lose! ${computerSelection} beats ${playerSelection}`;
+function getResult(playerSelection, computerSelection) {
+  if ((playerSelection == "rock" && computerSelection == "scissors") ||
+      (playerSelection == "paper" && computerSelection == "rock") ||
+      (playerSelection == "scissors" && computerSelection == "paper")) {
+      playerScore++;
+      return `You win! ${playerSelection} beats ${computerSelection}.`;
+      
+  }
+  else if (playerSelection == computerSelection) {
+    playerScore++;
+    computerScore++;  
+    return "It's a tie!";
+      
+  }
+  else {
+    computerScore++;  
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+}
 
-    console.log(playerSelection, computerSelection);
-    return result;
+function resetScore() {
+  playerScore=computerScore=0;
 }
